edf0f21c9b4338839f69590dbe7157cca6c53889
mohamed <mohamedmetoui2@gmail.com>
2019-12-07 19:17:15 +0100

    a

--- PIDEV-ejb/src/main/java/model/User.java
+++ PIDEV-ejb/src/main/java/model/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
+	@Id//
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int iduser;
 	
